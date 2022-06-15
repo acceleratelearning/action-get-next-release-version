a4cf650b1f28bbe8623a27bee7e3f4566bc36e12
Rob Cannon <rcannon@acceleratelearning.com>
2022-06-15 17:17:32 -0400

    Adding code to get releases

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {graphql} from '@octokit/graphql'
 
+type Node = {
+  name: string
+}
+
+type Releases = {
+  repository: {
+    releases: {
+      nodes: Node[]
+    }
+  }
+}
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const inputs = {
+      githubToken: core.getInput('github-token'),
+      majorMinorVersion: core.getInput('major-minor-version')
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(
+      `Looking for releases that match ${inputs.majorMinorVersion} ...`
+    ) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.setOutput('time', new Date().toTimeString())
+    //const octokit = github.getOctokit(inputs.githubToken)
+
+    const query = `query Releases($owner: String!, $repo: String!) {
+      repository(owner:$owner, name:$repo) {
+        releases(last: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+          nodes {
+            name
+          }
+        }
+      }
+    }`
+
+    const graphqlClient = graphql.defaults({
+      headers: {
+        authorization: `token ${inputs.githubToken}`
+      }
+    })
+
+    const pulls = await graphqlClient<Releases>(query, github.context.repo)
+
+    const matchingVersions = pulls.repository.releases.nodes.map(
+      node => node.name
+    )
+
+    core.debug(`Releases: ${matchingVersions}`)
+
+    core.info(`Releases: ${matchingVersions}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
